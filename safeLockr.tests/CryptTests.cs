5a2098bdcfb3c0ed5a1f5a98d169923fb9a61ec7
SodenSys <dennis.a.sys@gmail.com>
2024-02-07 06:47:48 +0100

    Converted app to web API #2

--- safeLockr.tests/CryptTests.cs
+++ safeLockr.tests/CryptTests.cs
@@ -1,48 +1,19 @@
-using static safeLockr.Program;
-
 namespace safeLockr.Tests
 {
-	public class CryptTests
-	{
-		[Fact]
-		public void EncryptTest()
-		{
-			// given
-			string input = "aaa123";
-			string expectedEncResult = "ggg123";
-
-			using (StringReader sr = new StringReader(input))
-			{
-				SetIn(sr);
-
-				// when
-				string actualEncResult = Encrypt();
-
-				// then
-				Assert.Equal(expectedEncResult, actualEncResult);
-			}
-		}
-		
+    public class CryptTests
+    {
         [Fact]
-        public void DecryptTest()
+        public void EncryptDecryptTest()
         {
             // given
-            string encryptedInput = "ggg123";
-            string expectedDecResult = "aaa123";
+            string originalInput = "aaa123";
 
-            using (StringReader sr = new StringReader(encryptedInput))
-            using (StringWriter sw = new StringWriter())
-            {
-                SetIn(sr);
-                SetOut(sw);
+            // when
+            string encryptedOutput = CryptFunc.Encrypt(originalInput);
+            string decryptedOutput = CryptFunc.Decrypt(encryptedOutput);
 
-                // when
-                Decrypt();
-
-                // then
-                string consoleOutput = sw.ToString().Trim();
-                Assert.Contains(expectedDecResult, consoleOutput);
-            }
+            // then
+            Assert.Equal(originalInput, decryptedOutput);
         }
-	}
+    }
 }
